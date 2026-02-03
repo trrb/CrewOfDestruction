ef2d747d3eb505db342e316c4c12df4c60e96903
trrb <tesakov.ruslan@gmail.com>
2026-02-03 21:54:21 +0300

    исправил таблицы блюд и фуд

--- dataalchemy/__init__.py
+++ dataalchemy/__init__.py
@@ -1,4 +1,4 @@
 from .db_engine import engine
 from .db_session import global_init, create_session
 from .models.base import Base
-from .models import Role, User
+from .models import Role, User, Dish, DishFood, Food
--- dataalchemy/models/__init__.py
+++ dataalchemy/models/__init__.py
@@ -1,2 +1,5 @@
 from .roles import Role
 from .users import User
+from .dishes import Dish
+from .food import Food
+from .dish_food import DishFood
